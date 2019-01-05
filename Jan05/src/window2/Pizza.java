0d8e769bfa42eaca1d2343c99ccb9389d45a90a4
son502 <46391632+son502@users.noreply.github.com>
2019-01-05 14:49:19 +0900

    Add files via upload

--- Jan05/src/window2/Pizza.java
+++ Jan05/src/window2/Pizza.java
@@ -1,12 +1,15 @@
 package window2;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -21,31 +24,50 @@ public class Pizza {
 	int bulgoki=0;
 	
 	JTextArea list;
+	private JLabel lblTotalSum;
+	private JButton btnNewButton;
 	
 	public Pizza(){
 		
 		f=new JFrame();
-		f.setSize(350,200);
+		f.setSize(336,240);
 		top=new JLabel("<<<<<<<자바피자에 오신것을 환영합니다>>>>>>>>");
 		count=new JLabel("갯수");
 		b1=new JButton("콤보피자");
+		b1.setBackground(Color.MAGENTA);
 		b2=new JButton("포테이토피자");
+		b2.setBackground(Color.MAGENTA);
 		b3=new JButton("불고기피자");
+		b3.setBackground(Color.MAGENTA);
 		total=new JTextField(20);
 		
 		FlowLayout flow=new FlowLayout();
-		f.setLayout(flow);
+		f.getContentPane().setLayout(flow);
 		list=new JTextArea(3,30);
+		list.setBackground(Color.YELLOW);
 		
+		f.getContentPane().add(top);
+		f.getContentPane().add(b1);
+		f.getContentPane().add(b2);
+		f.getContentPane().add(b3);
+		f.getContentPane().add(count);
+		f.getContentPane().add(total);
+		f.getContentPane().add(list);
 		
+		lblTotalSum = new JLabel("\uCD1D\uACC4");
+		lblTotalSum.setFont(new Font("HY헤드라인M", Font.PLAIN, 12));
+		lblTotalSum.setBackground(Color.BLACK);
+		lblTotalSum.setForeground(Color.RED);
+		f.getContentPane().add(lblTotalSum);
 		
-		f.add(top);
-		f.add(b1);
-		f.add(b2);
-		f.add(b3);
-		f.add(count);
-		f.add(total);
-		f.add(list);
+		btnNewButton = new JButton("Click Me!!!");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null,"정말 나를 누르셨군요!!");
+			}
+		});
+		btnNewButton.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 12));
+		f.getContentPane().add(btnNewButton);
 		
 		b1.addActionListener(new ActionListener() {
 			
@@ -57,7 +79,7 @@ public class Pizza {
 				
 				list.setText("콤보 개수: "+combo+"개\n"
 						+"포테이토 개수: "+potato+"개\n"
-						+"불고개 개수: "+bulgoki+"개"
+						+"불고기 개수: "+bulgoki+"개"
 						);
 			}
 		});
@@ -88,13 +110,9 @@ public class Pizza {
 			}
 		});
 		
-		
 		f.setVisible(true);
 		
-		
-		
 	}
-	
 
 	public static void main(String[] args) {
 
